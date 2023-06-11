ccaf3316e50ca75b387aec0e77281787e19c8e93
Ubuntu <ubuntu@ip-172-31-38-89.ap-northeast-2.compute.internal>
2023-06-11 17:28:29 +0000

    26/95

--- include/userprog/process.h
+++ include/userprog/process.h
@@ -11,4 +11,9 @@ void process_exit (void);
 void process_activate (struct thread *next);
 void argument_stack(char **argv, int argc, void **rsp);
 
+struct file *process_get_file(int fd);
+struct thread *get_child_process(tid_t pid);
+void process_close_file(int fd);
+int process_add_file(struct file *f);
+
 #endif /* userprog/process.h */
